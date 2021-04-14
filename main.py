1178abe3abff1567fe746a1f448fc506ba62ba3c
Julio R <cesar32az@gmail.com>
2021-04-13 22:18:20 -0600

    add uvicorn.run

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
-
+import uvicorn
 app = FastAPI()
 
 
@@ -45,5 +45,10 @@ class Blog(BaseModel):
 
 
 @app.post('/blog')
-def create_blog(blog:Blog):
+def create_blog(blog: Blog):
     return {'data': f'Blog is created with title as {blog.title}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="127.0.0.1", port=4000, reload=True)
+
