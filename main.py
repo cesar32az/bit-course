ccccdb903a4b014c92c4c613aedaf8ca29f80835
Julio R <cesar32az@gmail.com>
2021-04-13 21:57:45 -0600

    learn path operations and parameters

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,5 +10,30 @@ def index():
     return {'data': {'name': 'Julio R'}}
 
 
-def about():
-    return {'data': 'about page'}
+@app.get('/blog')
+def blogs(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+
+    if published:
+        # only get published blogs defined by the limit
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        # only get blogs defined by the limit
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit=10):
+    # fetch comments of blog with id = id
+    return {'data': {'limit': limit, 'comments': ['c1', 'c2']}}
+
